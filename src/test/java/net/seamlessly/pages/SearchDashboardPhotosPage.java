a729dc87f727db3404a2526a3f84cb425de6e522
nihatgokay <nihatgokay@gmail.com>
2021-08-25 18:20:26 +0300

    first AC added

--- src/test/java/net/seamlessly/pages/SearchDashboardPhotosPage.java
+++ src/test/java/net/seamlessly/pages/SearchDashboardPhotosPage.java
@@ -7,4 +7,10 @@ public class SearchDashboardPhotosPage extends BasePage {
 
     @FindBy(xpath = "(//form/input)[1]")
     public WebElement textbox;
+
+    @FindBy(xpath = "(//ul/li[1]/a)[3]")
+    public WebElement clickButton;
+
+    @FindBy(xpath = "(//header)[2]//h2")
+    public WebElement detail;
 }
--- src/test/java/net/seamlessly/stepdefinitions/SearchDashboardPhotosDefs.java
+++ src/test/java/net/seamlessly/stepdefinitions/SearchDashboardPhotosDefs.java
@@ -2,10 +2,10 @@ package net.seamlessly.stepdefinitions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import net.seamlessly.pages.BasePage;
 import net.seamlessly.pages.HomePage;
 import net.seamlessly.pages.SearchDashboardPhotosPage;
 import net.seamlessly.utilities.BrowserUtils;
+import org.junit.Assert;
 
 public class SearchDashboardPhotosDefs {
 
@@ -19,16 +19,20 @@ public class SearchDashboardPhotosDefs {
     @When("User can write {string} into the text box")
     public void user_can_write_into_the_text_box(String filename) {
         new SearchDashboardPhotosPage().textbox.sendKeys(filename);
+        BrowserUtils.waitFor(5);
     }
 
     @When("User can click on the file name")
     public void user_can_click_on_the_file_name() {
-        // Write code here that turns the phrase above into concrete actions
+        new SearchDashboardPhotosPage().clickButton.click();
+        BrowserUtils.waitFor(5);
 
     }
-    @Then("User can see the details side page of the file")
-    public void user_can_see_the_details_side_page_of_the_file() {
-        // Write code here that turns the phrase above into concrete actions
 
+    @Then("User can see the details side page of the {string}")
+    public void user_can_see_the_details_side_page_of_the(String filename) {
+        System.out.println("new SearchDashboardPhotosPage().detail.getText() = " + new SearchDashboardPhotosPage().detail.getText());
+        Assert.assertTrue(new SearchDashboardPhotosPage().detail.getText().contains(filename));
     }
+
 }
