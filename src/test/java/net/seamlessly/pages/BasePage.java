c8684ff93557db315e01a887b1b48740c12b7684
hinan5534 <inan5534@gmail.com>
2021-08-25 20:23:14 +0300

    Calendar is located

--- src/test/java/net/seamlessly/pages/BasePage.java
+++ src/test/java/net/seamlessly/pages/BasePage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public abstract class BasePage {
+public class BasePage {
 
     @FindBy(xpath="//div[@class='avatardiv avatardiv-shown']")
     public WebElement avatarIcon;
@@ -23,6 +23,9 @@ public abstract class BasePage {
 
     @FindBy(xpath="//div[@class='logo logo-icon']/h1")
     public WebElement logoLabel;
+
+    @FindBy(xpath = "(//a[@aria-label='Calendar'])[1]")
+    public WebElement Calendar;
 
 
     public BasePage() {
