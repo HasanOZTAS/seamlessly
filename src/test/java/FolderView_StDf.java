37456063855defcc76dffb5ac44d555b15ec1231
Mustafa Ubeyde BUYUKAY <81827430+mustafa-glitch@users.noreply.github.com>
2021-08-27 14:04:06 +0300

    First Commit-created my feature file-MB

---
+++ src/test/java/FolderView_StDf.java
@@ -0,0 +1,49 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class FolderView_StDf {
+    @And("the user clicks on name")
+    public void theUserClicksOnName() {
+        
+    }
+
+    @When("the user is on dashboard")
+    public void theUserIsOnDashboard() {
+        
+    }
+
+    @Then("the view of folders should change by name")
+    public void theViewOfFoldersShouldChangeByName() {
+        
+    }
+
+    @And("the user clicks on size")
+    public void theUserClicksOnSize() {
+        
+    }
+
+    @Then("the order of the folders should change by size")
+    public void theOrderOfTheFoldersShouldChangeBySize() {
+        
+    }
+
+    @And("the user clicks on modified")
+    public void theUserClicksOnModified() {
+        
+    }
+
+    @Then("the order of the folders should change by date")
+    public void theOrderOfTheFoldersShouldChangeByDate() {
+        
+    }
+
+    @And("the user clicks on toggle view")
+    public void theUserClicksOnToggleView() {
+        
+    }
+
+    @Then("the order of the folders should change by view")
+    public void theOrderOfTheFoldersShouldChangeByView() {
+    }
+}
---
+++ src/test/resources/features/FolderView.feature
@@ -0,0 +1,26 @@
+Feature: User Folder view functionality
+
+  Background: the user is logged in
+
+    Scenario:
+    When the user is on dashboard
+    And the user clicks on name
+    Then the view of folders should change by name
+
+      Scenario:
+        When the user is on dashboard
+        And the user clicks on size
+        Then the order of the folders should change by size
+
+
+        Scenario:
+          When the user is on dashboard
+          And the user clicks on modified
+          Then the order of the folders should change by date
+
+
+          Scenario: User can change the folder view
+
+            When the user is on dashboard
+            And the user clicks on toggle view
+            Then the order of the folders should change by view
