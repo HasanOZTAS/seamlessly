143409f9348c62c170c9de98a504d226aaebfd37
johnolliverjr <johnolliverjr@gmail.com>
2021-08-28 21:15:47 +0300

    first ac done second ac half done

--- src/test/java/net/seamlessly/pages/FilesPage.java
+++ src/test/java/net/seamlessly/pages/FilesPage.java
@@ -22,8 +22,34 @@ public class FilesPage extends BasePage{
     @FindBy(xpath = "//span[@class='icon icon-rename']")
     public WebElement rename;
 
-    @FindBy(xpath = "//span[@class='innernametext']")
-    public WebElement renameBox;
+    @FindBy(xpath = "(//*[.='please do not edit or delete'])[4]")
+    public WebElement fileName;
+
+    @FindBy(xpath = "(//*[.='new name'])[4]")
+    public WebElement reNamedFile;
+
+    @FindBy(xpath = "(//span[@class='icon icon-more'])[3]")
+    public WebElement newName3dot;
+
+    @FindBy(xpath = "//span[@class='icon icon-details']")
+    public WebElement datails;
+
+    @FindBy(id = "commentsTabView")
+    public WebElement commentsTab;
+
+    @FindBy(xpath = "//div[@class='message']")
+    public WebElement commentsTextBox;
+
+    @FindBy(xpath = "//div[.='some comments']")
+    public WebElement addedComment;
+
+    @FindBy(xpath = "//a[@class='action more icon icon-more has-tooltip']")
+    public WebElement comments3Dot;
+
+    @FindBy(xpath = "//div[@class='commentsModifyMenu popovermenu bubble menu menu-left']//span[@class='icon icon-delete']")
+    public WebElement deleteComment;
+
+
 
 
     public FilesPage() {
--- src/test/java/net/seamlessly/stepdefinitions/FilesStepDefs.java
+++ src/test/java/net/seamlessly/stepdefinitions/FilesStepDefs.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.interactions.SendKeysAction;
 public class FilesStepDefs {
     @When("the user clicks on an existing file's add to favorites under its own three dots menu")
     public void the_user_clicks_on_an_existing_file_s_add_to_favorites_under_its_own_three_dots_menu() {
-    FilesPage filesPage = new FilesPage();
+        FilesPage filesPage = new FilesPage();
         filesPage.filesThreeDotMenu.click();
         filesPage.addToFavorites.click();
         BrowserUtils.waitFor(2);
@@ -21,8 +21,8 @@ public class FilesStepDefs {
 
     @When("the user navigates to favorites tab")
     public void the_user_navigates_to_favorites_tab() {
-       new FilesPage().favoritesTab.click();
-       BrowserUtils.waitFor(2);
+        new FilesPage().favoritesTab.click();
+        BrowserUtils.waitFor(2);
     }
 
     @Then("the user can verify that the file has been added under the favorites tab")
@@ -43,44 +43,57 @@ public class FilesStepDefs {
 
     @When("the user renames the file")
     public void the_user_renames_the_file() {
-
-     //   new FilesPage().renameBox.sendKeys("new name" + Keys.ENTER);
+        Driver.get().switchTo().activeElement().sendKeys("new name" + Keys.ENTER);
         BrowserUtils.waitFor(2);
+        Driver.get().navigate().refresh();
     }
 
     @Then("the user can verify that the file has been renamed")
     public void the_user_can_verify_that_the_file_has_been_renamed() {
-
+        FilesPage filesPage = new FilesPage();
+        System.out.println("reNamedFile.getText() = " + filesPage.reNamedFile.getText());
+        Assert.assertEquals("new name", filesPage.reNamedFile.getText());
+        BrowserUtils.waitFor(2);
     }
 
     @When("the user clicks on an existing file's details from its own three dots menu")
     public void the_user_clicks_on_an_existing_file_s_details_from_its_own_three_dots_menu() {
-
+        FilesPage filesPage = new FilesPage();
+        filesPage.newName3dot.click();
+        filesPage.datails.click();
     }
 
     @When("the user navigates to comments tab")
     public void the_user_navigates_to_comments_tab() {
+        new FilesPage().commentsTab.click();
 
     }
 
     @When("put some comments")
     public void put_some_comments() {
-
+        new FilesPage().commentsTextBox.sendKeys("some comments" + Keys.ENTER);
+        BrowserUtils.waitFor(2);
     }
 
     @Then("the user can verify that comment has been added")
     public void the_user_can_verify_that_comment_has_been_added() {
+        Assert.assertEquals("some comments", new FilesPage().addedComment.getText());
 
     }
 
     @When("click on any comment's own three dot menu and select delete")
     public void click_on_any_comment_s_own_three_dot_menu_and_select_delete() {
-
+    BrowserUtils.waitFor(1);
+        new FilesPage().comments3Dot.click();
+    BrowserUtils.waitFor(1);
+    new FilesPage().deleteComment.click();
+    BrowserUtils.waitFor(1);
     }
 
     @Then("the user can verify that comment has been deleted")
     public void the_user_can_verify_that_comment_has_been_deleted() {
-
+    BrowserUtils.waitFor(2);
+        Assert.assertTrue(!new FilesPage().addedComment.isDisplayed());
     }
 
 }
