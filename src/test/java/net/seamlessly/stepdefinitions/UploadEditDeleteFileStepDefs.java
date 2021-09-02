3c9623df1e088e09c5b11c5421208a7b0f6d1dfc
Azat <bayramov.azat.tkm@gmail.com>
2021-09-02 04:28:21 +0300

    FilesPage created

--- src/test/java/net/seamlessly/stepdefinitions/UploadEditDeleteFileStepDefs.java
+++ src/test/java/net/seamlessly/stepdefinitions/UploadEditDeleteFileStepDefs.java
@@ -1,4 +1,57 @@
 package net.seamlessly.stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class UploadEditDeleteFileStepDefs {
+
+    @Given("the user navigate to {string} {string}")
+    public void the_user_navigate_to(String string, String string2) {
+
+    }
+
+
+    @When("the user click on {string} then click {string}")
+    public void the_user_click_on_then_click(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user navigate testJPGFile.jpg and upload it")
+    public void the_user_navigate_testJPGFile_jpg_and_upload_it() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user should see testJPGFile.jpg on page")
+    public void the_user_should_see_testJPGFile_jpg_on_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+    @Given("the user navigate to {string} {string}")
+    public void the_user_navigate_to(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+    @When("the user click on {string} then click {string}")
+    public void the_user_click_on_then_click(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user navigate testTXTFile.txt and upload it")
+    public void the_user_navigate_testTXTFile_txt_and_upload_it() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user should see testTXTFile.txt on page")
+    public void the_user_should_see_testTXTFile_txt_on_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
